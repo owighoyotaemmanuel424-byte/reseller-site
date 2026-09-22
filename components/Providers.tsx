ecd678ce55d3a4a59ed458e05e869f7cf960ef3b
owighoyotaemmanuel424-byte <owighoyotaemmanuel424@gmail.com>
2026-09-22 20:06:05 +0100

    feat: migrate MultiKartX runtime from Convex to Neon

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,14 +1,2 @@
 "use client";
-
-import { ConvexProvider } from "convex/react";
-import { ConvexReactClient } from "convex/react";
-
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <ConvexProvider client={convex}>
-      {children}
-    </ConvexProvider>
-  );
-}
+export function Providers({children}:{children:React.ReactNode}){return <>{children}</>}
