4e1ac0da5efa61eaf65eeb6f1af9ae4523950051
owighoyotaemmanuel424-byte <owighoyotaemmanuel424@gmail.com>
2026-09-23 01:03:18 +0100

    feat: wire payment module into API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from './auth/auth.module';
 import { WalletModule } from './wallet/wallet.module';
+import { PaymentModule } from './payments/payment.module';
 
-@Module({ imports: [JwtModule.register({}), AuthModule, WalletModule] })
+@Module({ imports: [JwtModule.register({}), AuthModule, WalletModule, PaymentModule] })
 export class AppModule {}
